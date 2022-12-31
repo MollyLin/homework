593a750d10d33896c9d1b48e02520cd24106dee6
Molly <mollylin2014@gmail.com>
2022-12-31 14:36:27 +0800

    init 編譯 ts 環境

---
+++ index.js
@@ -0,0 +1,9 @@
+var message = 'Hello World!';
+function say(something) {
+    console.log(something);
+}
+say(message);
+// => Hello World
+// const message = 'Hello World!';
+// Should Raise Error!
+// console.log(message.touppercase());
