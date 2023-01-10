8b5c2f5a250624e861e6ac9f20f2516022302c4d
Molly Lin <mollylin2014@gmail.com>
2023-01-10 17:59:28 +0800

    complete 策略模式

--- homework.ts
+++ homework.ts
@@ -1,8 +1,13 @@
 const creditCard = (amount: number) => {
-  return Math.floor(amount * 0.8);
+  console.log(`信用卡結帳總金額為 ${Math.floor(amount * 0.8)}`);
 };
+
 const linePay = (amount: number) => {
-  console.log(amount);
+  let discount = 0;
+  if (amount >= 1000) {
+    discount = Math.floor(amount / 1000) * 100;
+  }
+  console.log(`LinePay 結帳總金額為 ${amount - discount}`);
 };
 const priceStrategies = {
   'creditCard': creditCard,
@@ -12,4 +17,5 @@ const checkout = (payment: string, amount: number): number => {
   return priceStrategies[payment](amount);
 };
 
-checkout('creditCard', 10000);
+checkout('creditCard', 6000);
+checkout('linePay', 6000);
