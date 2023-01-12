d96daea3d61af17006eda85fca00273f131285f6
Molly <molly.lin@senao.com.tw>
2023-01-12 17:25:36 +0800

    整理策略模式 note

--- index.ts
+++ index.ts
@@ -1,46 +1,10 @@
-
+// 工廠模式
 /** 
- * 1. 設置原始資料: 明細金額。
- * 2. 封裝運算模式
- * 3. 當 user 呼叫計算時，將請求委託給之前存放好的策略物件
- * */
+ * source: [OOP doc from google drive](https://drive.google.com/drive/folders/1McWN1mSwTCRme0gui3bPW0O_4xF5amiI)
+ * 原來的程式碼不需要增加 if else 判斷,已經將變化封裝成相同 interface 的不同 class
+ * 程式碼只與 interface 耦合,不與 function 耦合 ; 呼叫端只認 interface,不認 function,只要 interface 不變,function 呼叫端不必修改
+ * 相同的物件可抽象化成 class
+ * 物件裡的 function 稱作方法 (Method): bracket notation OR dot notation
+ * [qmo's nbaFactory.ts](https://github.com/QmoGamer/oop_homework/blob/homework/nbaFactory.ts)
 
-// const linepayDiscount = parseInt(amount / 1000) * 100;
-// Math.floor(amount / 1000) * 100
-// const creditDiscount = 
-// creditRule & linepayRule
-
-// create a 接受 all payments 及 amount 的輸入，輸出依不同 payments 最後算出符合的折扣結果。
-// checkoutProcess();
-// 目的: 不需動結構即可增加新折扣規則
-// user pick linepay > if (amount > 1000 ) 呼叫 linepayDiscount()
-
-// 策略物件:
-/**
- * const payments = {
- *  creditCard: function() {},
- *  linepay: function() {},
- * }
- * or
- * creditCard fn()
- * linepay fn()
- * checkout fn()
- * 
- * // const context = function()
- * checkout('creditCard', 10000)
- * checkout('linePay', 10000)
- */
-
-const message = 'Hello World!';
-
-function say(something: string): void {
-  console.log(something);
-}
-
-say(message);
-// => Hello World
-
-// const message = 'Hello World!';
-
-// Should Raise Error!
-// console.log(message.touppercase());
+*/
