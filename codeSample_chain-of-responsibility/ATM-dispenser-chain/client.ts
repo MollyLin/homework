f9c1aa0dc6de17a07b53c326368bdb3256688326
Molly <molly.lin@senao.com.tw>
2023-02-22 15:15:09 +0800

    arrange and remove reference code file ; edit tsconfig

--- codeSample_chain-of-responsibility/ATM-dispenser-chain/client.ts
+++
@@ -1,8 +0,0 @@
-import process from 'process';
-import ATMDispenserChain from "./atm-dispenser-chain";
-
-const ATM = new ATMDispenserChain();
-console.log(`Enter amount to withdrawal:`);
-process.stdin.on('data', (data: string) => {
-
-});
